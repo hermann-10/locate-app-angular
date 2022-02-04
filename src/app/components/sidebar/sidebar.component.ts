3693cd715261cfd7ff313ff1820f6a34feacbdb5
Hermann Manuel <hermann.manuel@hotmail.com.com>
2022-02-04 00:10:37 -0800

    Creation of the function to disconnect a user from the sidebar

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from 'src/app/shared/services/auth.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor(private authService: AuthService, public auth: AngularFireAuth) { }
+
+  ngOnInit() {
+  }
+
+  logout(){
+    this.authService.SignOut();
+  }
+
+}
