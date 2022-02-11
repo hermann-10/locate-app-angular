3078d0f0fe67f5d2347eea6a0afee2ebb46a09bf
Hermann Manuel <hermann.manuel@hotmail.com.com>
2022-02-11 13:48:43 +0100

    Treatment of the display or not of the sidebar according to if the user is connected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AuthService } from './shared/services/auth.service';
+import firebase from 'firebase/compat/app';
+
 
 @Component({
   selector: 'app-root',
@@ -9,15 +11,34 @@ import { AuthService } from './shared/services/auth.service';
 })
 export class AppComponent {
   title = 'Angular-Authentication';
+  status: boolean = false;
+  isConnected!: boolean;
+  userData: any; // Save logged in user data
 
-     status: boolean = false;
+  
+
   clickEvent(){
       this.status = !this.status;       
   }
   
-  constructor(private authService: AuthService, public auth: AngularFireAuth) { }
+  constructor(public authService: AuthService, public auth: AngularFireAuth,     public afAuth: AngularFireAuth, // Inject Firebase auth service
+    ) { 
+    }
 
   ngOnInit() {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.userData = user;
+        this.isConnected = this.authService.isLoggedIn;
+        console.log('Logged ?',  this.authService.isLoggedIn)
+        console.log('user.emailVerified : ', user.emailVerified);
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        //JSON.parse(localStorage.getItem('user'));
+      } else {
+        localStorage.setItem('user', '');
+        //JSON.parse(localStorage.getItem('user'));
+      }
+    })
   }
 
   logout(){
