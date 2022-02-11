cd6032174313b0ddc44a3c5761078b3763a2ae0f
Hermann Manuel <hermann.manuel@hotmail.com.com>
2022-02-11 17:01:25 +0100

    Work on solving the problem of hiding the sidebar when the user logs out. I saw that using ".navigateByUrl" makes the page reload. So it hides well the sidebar when the user is not connected. I'll look for a better alternative though.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
@@ -8,19 +9,43 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  subscription1$!: Subscription;
+  status: boolean = false;
+  isConnected!: boolean;
+  userData: any; // Save logged in user data
 
-   status: boolean = false;
+  
   clickEvent(){
       this.status = !this.status;       
   }
   
-  constructor(private authService: AuthService, public auth: AngularFireAuth) { }
+  constructor(public authService: AuthService, public auth: AngularFireAuth,     public afAuth: AngularFireAuth, // Inject Firebase auth service
+    ) { }
 
   ngOnInit() {
+   this.subscription1$ = this.afAuth.authState.subscribe(user => {
+     console.log(' this.subscription1$',  this.subscription1$);
+      if (user) {
+        this.userData = user;
+        this.isConnected = this.authService.isLoggedIn;
+        console.log('Logged ?',  this.authService.isLoggedIn)
+        console.log('user.emailVerified : ', user.emailVerified);
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        //JSON.parse(localStorage.getItem('user'));
+      } else {
+        localStorage.setItem('user', '');
+        //JSON.parse(localStorage.getItem('user'));
+      }
+    })
   }
 
   logout(){
     this.authService.SignOut();
   }
+
+  ngOnDestroy() {
+    this.subscription1$.unsubscribe();
+    console.log(' this.subscription1$ unsubscribe ?',  this.subscription1$);
+}
 
 }
